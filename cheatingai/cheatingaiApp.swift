d45cc33fde26f25d608328b09ff8fd9a1a9f456a
Shreyas Gurav <shrreyasgurav@gmail.com>
2025-08-09 14:32:01 +0530

    Initial Commit

---
+++ cheatingai/cheatingaiApp.swift
@@ -0,0 +1,17 @@
+//
+//  cheatingaiApp.swift
+//  cheatingai
+//
+//  Created by Shreyas Gurav on 09/08/25.
+//
+
+import SwiftUI
+
+@main
+struct cheatingaiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ cheatingaiTests/cheatingaiTests.swift
@@ -0,0 +1,17 @@
+//
+//  cheatingaiTests.swift
+//  cheatingaiTests
+//
+//  Created by Shreyas Gurav on 09/08/25.
+//
+
+import Testing
+@testable import cheatingai
+
+struct cheatingaiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
